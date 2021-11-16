08648bf2198eb6113f358fb6a72b2897f46f3c0b
hessonit <hessonit@gmail.com>
2021-11-16 23:33:38 +0100

    indentation issue and adding connection test

--- php/index.php
+++ php/index.php
@@ -5,3 +5,19 @@ echo sprintf('Hello %s!', $name);
 
 $otherName = getenv('DB_NAME', true) ?: 'UNKNOWN';
 echo sprintf('Hello %s!', $otherName);
+
+
+$dbserver = getenv('DB_NAME', true);
+$dbuser = getenv('DB_USER', true);
+$dbpassword = getenv('DB_PASS', true);
+// In a production blog, we would not store the MySQL
+// password in the document root. Instead, we would store it in a
+// configuration file elsewhere on the web server VM instance.
+$conn = new mysqli($dbserver, $dbuser, $dbpassword);
+if (mysqli_connect_error()) {
+        echo ("Database connection failed: " . mysqli_connect_error());
+} else {
+        echo ("Database connection succeeded.");
+}
+
+?>
